bd0c911acc878885f9475fa5bfcafa55b9ba1f28
Tim Welch <tim.j.welch@gmail.com>
2023-01-09 15:33:58 -0800

    remove errant copy

--- copy/open_shell.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Opens a shell in a heatmap contain, with projects folder mounted to /projects
-
-docker run --rm -it \
-    --volume "$(pwd)"/projects:/projects \
-    seasketch/heatmap:latest \
-    /bin/bash
